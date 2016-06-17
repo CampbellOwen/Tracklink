39e8ca804cdb72397299ecf67ca924ba35991f11
ocampbel <ocampbel@sfu.ca>
2016-06-17 16:14:09 -0700

    added index to email column for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160617210406) do
+ActiveRecord::Schema.define(version: 20160617231254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20160617210406) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
   create_table "widgets", force: :cascade do |t|
     t.string   "name"
