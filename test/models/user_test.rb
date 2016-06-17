4589c9839e4d026ba5472f8c1f61fe162262e8a0
ocampbel <ocampbel@sfu.ca>
2016-06-17 14:19:07 -0700

    updated user test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,14 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+      @user = User.new(email: "user@example.com", password: "foobar", password_confirmation: "foobar", level: "user")
+  end
+    test "email addresses should be unique" do
+        duplicate_user = @user.dup
+        duplicate_user.email = @user.email.upcase
+        @user.save
+        assert_not duplicate_user.valid?
+  end
 end
