e26ed8ddbec9296ef218d9e6b4372d3fc7a1284c
ocampbel <ocampbel@sfu.ca>
2016-06-17 16:39:50 -0700

    Updated fixtures so tests pass

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,12 +6,22 @@ class UserTest < ActiveSupport::TestCase
   # end
 
   def setup
-      @user = User.new(email: "user@example.com", password: "foobar", password_confirmation: "foobar", level: "user")
+      @user = User.new(email: "user1@example.com", password: "foobar", password_confirmation: "foobar", level: "user")
   end
-    test "email addresses should be unique" do
+  test "email addresses should be unique" do
         duplicate_user = @user.dup
         duplicate_user.email = @user.email.upcase
         @user.save
         assert_not duplicate_user.valid?
+  end
+
+  test "password should be nonblank" do
+      @user.password = @user.password_confirmation = " " * 6
+      assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+      @user.password = @user.password_confirmation = "a" * 5
+      assert_not @user.valid?
   end
 end
