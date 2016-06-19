dcfba2a0183540c7ab9746ef3b18577cedb7e12a
Ankit Dassor <adassor@sfu.ca>
2016-06-19 10:46:33 -0700

    Modified HTML into table, also testing pushing

--- db/schema.rb
+++ db/schema.rb
@@ -16,11 +16,6 @@ ActiveRecord::Schema.define(version: 20160618053202) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "students", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "level"
@@ -31,13 +26,5 @@ ActiveRecord::Schema.define(version: 20160618053202) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-
-  create_table "widgets", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.integer  "stock"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
 end
