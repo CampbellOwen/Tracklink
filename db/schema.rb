01fd0c387dd38269749b519cfdb9925107cfa9cc
Ankit Dassor <adassor@sfu.ca>
2016-07-07 22:31:42 -0700

    started refactoring css, made login/signup consistent with rest of app

--- db/schema.rb
+++ db/schema.rb
@@ -24,11 +24,6 @@ ActiveRecord::Schema.define(version: 20160707051457) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "students", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.datetime "created_at",      null: false
@@ -39,13 +34,5 @@ ActiveRecord::Schema.define(version: 20160707051457) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-
-  create_table "widgets", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.integer  "stock"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
 end
