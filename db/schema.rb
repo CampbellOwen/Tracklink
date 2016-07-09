bfc66d19811c8de6c8178d7fb0df689cf3e92ab6
Ankit Dassor <adassor@sfu.ca>
2016-07-09 15:57:21 -0700

    partials for placeholders for html readability and some css interactive elements added

--- db/schema.rb
+++ db/schema.rb
@@ -45,11 +45,6 @@ ActiveRecord::Schema.define(version: 20160709093123) do
     t.string   "Routes"
   end
 
-  create_table "students", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.datetime "created_at",      null: false
@@ -60,13 +55,5 @@ ActiveRecord::Schema.define(version: 20160709093123) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-
-  create_table "widgets", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.integer  "stock"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
 end
