53540d27367c1a758ffd657e705e5cf6755fbc99
Ankit Dassor <adassor@sfu.ca>
2016-07-10 22:19:31 -0700

    js to return nearby stops based on lat/long

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,21 +1,20 @@
 function getRoutes(lat_long){
 	var lat = lat_long.lat().toFixed(6);
 	var lng = lat_long.lng().toFixed(6);
-	var url = "http://api.translink.ca/rttiapi/v1/stops?apikey=QUprTm0ALxtTt4npEjl6&lat=" + lat + "&long=" + lng;
-	document.getElementById('debug1').innerHTML = url;
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', url, true);
-
-	xhr.onreadystatechange = function(){
-		if(xhr.readyState == 4 && xhr.status == 200){
-			var response = JSON.parse(xhr.responseText);
-			document.getElementById('routes_ex').innerHTML = response.StopNo[0];
-		}
-	}
-	xhr.send();
+	var url = "http://api.translink.ca/rttiapi/v1/stops?apikey=QUprTm0ALxtTt4npEjl6&lat=" + lat + "&long=" + lng; 
+	var yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from xml where url="' + url + '"') + '&format=xml&callback=?';
+	//document.getElementById('debug1').innerHTML = url;
+	$.getJSON(yql, function (data) {
+	   	var xml = $.parseXML(data.results[0]);
+	   	$xml = $(xml);
+	   	$("#routes_ex").html("<b>Stops Nearby:</b><br>");
+	   	$xml.find("Stop").each( function() {
+	      	var stop = $(this).find("StopNo");
+	       	console.log(stop.text());
+	       	$("#routes_ex").append(stop.text() + "<br>");
+   		});
+});
 }
-
-
 
 function initMap() {
 	var map = new google.maps.Map(document.getElementById('map'), {
